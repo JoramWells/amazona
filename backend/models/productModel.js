21738b4e576250a7289b51f5983731af05d9c269
bramuel <jorammanoah1@gmail.com>
2021-02-17 05:41:42 -0800

    Changes

---
+++ backend/models/productModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  brand: { type: String, required: true },
+  price:{type:Number, default:0, required:true},
+  category: { type: String, required: true },
+  countInStock: { type: Number, default:0, required: true },
+  description: { type: String, required: true },
+  rating: { type: Number, default:0},
+  numReviews: { type: Number, default:0  },
+});
+
+const productModel = mongoose.model("Product", productSchema);
+
+module.exports = productModel;
